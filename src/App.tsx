a9bff70802e63808e5740cf84c7bfd61f90cabbf
Lewis Battersby <lpb303@gmail.com>
2023-03-28 20:26:46 +0100

    feat(twordl-tiles): adds base twordle tiles and base grid css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import './components/TwordleGrid/TwordleGrid.css';
+import TwordleTile from "./components/TwordleTile/TwordleTile";
 
 function App() {
   return (
     <div className="App">
+      <div className="TwordleGrid">
+        <TwordleTile letter={"a"} state={"correct-place"}/>
+        <TwordleTile state={"incorrect"} letter={"z"}/>
+        <TwordleTile disabled={true}/>
+        <TwordleTile state={"wrong-place"} letter={"b"}/>
+      </div>
     </div>
   );
 }
---
+++ src/components/TwordleTile/TwordleTile.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './TwordleTile.css';
+
+type States = "correct-place" | "wrong-place" | "incorrect";
+
+const TwordleTile = (props: {disabled : boolean, state: States, letter: string}) => {
+  const { state, disabled, letter } = props;
+
+  const backgroundColor = () => {
+    if (disabled) {
+      return "#333333";
+    }
+    else {
+      switch (state) {
+        case "correct-place":
+          return "#0c7a30";
+        case "incorrect":
+          return "#777777";
+        case "wrong-place":
+          return "#c58c0c";
+      }
+    }
+  };
+
+  return (
+    <div className="TwordleTile" style={{backgroundColor: backgroundColor()}}>
+      {!disabled ? letter : null}
+    </div>
+  )
+}
+
+TwordleTile.defaultProps = {
+  disabled: false,
+  state: "incorrect",
+  letter: ""
+}
+
+export default TwordleTile;
